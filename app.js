990b4eec9e2a4812242ad89b6f603121213ebf7c
JoyKarmoker <Joykarmoker353@gmail.com>
2022-04-03 23:55:11 +0600

    Completed the assignment 2

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express()
 const port = 3000
 const dishRouter = require(__dirname + '/dishRouter');
 const promoRouter = require(__dirname + '/promoRouter');
 const leaderRouter = require(__dirname + '/leaderRouter');
- 
+mongoose.connect('mongodb://localhost:27017/assignmentdb');
+
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
